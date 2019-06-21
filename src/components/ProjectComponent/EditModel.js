e2b84fb69eed226b4afcabe6eee46f503dca5520
Fathima Sajitha <fathi.saji8@gmail.com>
2019-06-21 16:31:35 +0530

    Page Designing and Post,Delete,Get Integration

--- src/components/ProjectComponent/EditModel.js
+++ src/components/ProjectComponent/EditModel.js
@@ -1,106 +1,162 @@
-import { Modal, Form, Row, Col, Input, Icon, Checkbox} from 'antd';
-import React from 'react';
+import { Modal, Form, Row, Col, Input, Icon, DatePicker } from "antd";
+import React from "react";
 function onChange(e) {
   console.log(`checked = ${e.target.checked}`);
 }
 export default class Model extends React.Component {
   state = {
-   
     disabled: true,
-    visible: false,
+    visible: false
   };
-
 
   toggleDisable = () => {
     this.setState({ disabled: !this.state.disabled });
   };
 
   onChange = e => {
-    console.log('checked = ', e.target.checked);
+    console.log("checked = ", e.target.checked);
     this.setState({
-      checked: e.target.checked,
+      checked: e.target.checked
     });
   };
 
   showModal = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   handleOk = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
   handleCancel = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
-render() {
+  render() {
     return (
       <div>
-        <Icon 
-        type="edit" 
-        onClick={this.showModal}
-        style={{fontSize:'18px', color:'Blue'}} />
-        <br/>
+        <Icon
+          type="edit"
+          onClick={this.showModal}
+          style={{ fontSize: "18px", color: "Blue" }}
+        />
+        <br />
         <Modal
           title="Edit Project"
           visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
+          okText="Update"
         >
-        <Form layout="vertical">
-<Row>
-    
-    <Col  span={24} style={{padding: "5px"}}>
-        <Form.Item label="Project Name">
-        <Input placeholder="ProjectName"/>
-         </Form.Item>{" "}
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-    <div>
-        
-        <p>
-          
-          <Checkbox
-            style={{ marginLeft: '10px' }}
-           
-            onClick={this.toggleDisable}
-            onChange={onChange}
+          <Form layout="vertical">
+            <Row>
+              <Col span={24} style={{ padding: "5px" }}>
+                <Form.Item label="Project Name">
+                  <Input
+                    placeholder="ProjectName"
+                    value={this.state.projectName}
+                    onChange={this.onChangeprojectName}
+                  />
+                </Form.Item>{" "}
+              </Col>
+            </Row>
 
-          >Edit
-            {!this.state.disabled }
-          </Checkbox>
-        
-        </p>
-      </div>
-    </Col>
-  
-    
-    <Col  span={24} style={{padding: "5px"}}>
-    
-        <Form.Item>
-        <Input 
-        disabled={this.state.disabled}
-        onChange={this.onChange}
-        placeholder="Abbrevation"   />
-         </Form.Item>{" "}
-    </Col>
+            <Row>
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Type">
+                  <Input
+                    placeholder="Type"
+                    value={this.state.type}
+                    onChange={this.onChangeType}
+                  />
+                </Form.Item>{" "}
+              </Col>
 
-   </Row>
-  
-      </Form>
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Start Date" style={{ marginBottom: 0 }}>
+                  <span
+                    style={{
+                      display: "inline-block",
+                      width: "24px",
+                      textAlign: "center"
+                    }}
+                  />
+                  <Form.Item
+                    style={{
+                      display: "inline-block",
+                      width: "calc(90% - 12px)"
+                    }}
+                  >
+                    <DatePicker onChange={this.onChangeStartDate} />
+                  </Form.Item>
+                </Form.Item>
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="End Date" style={{ marginBottom: 0 }}>
+                  <span
+                    style={{
+                      display: "inline-block",
+                      width: "24px",
+                      textAlign: "center"
+                    }}
+                  />
+                  <Form.Item
+                    style={{
+                      display: "inline-block",
+                      width: "calc(90% - 12px)"
+                    }}
+                  >
+                    <DatePicker
+                      // value={this.state.endDate}
+                      onChange={this.onChangeEndDate}
+                    />
+                  </Form.Item>
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Duration">
+                  <Input
+                    placeholder="Duration"
+                    value={this.state.duration}
+                    onChange={this.onChangeDuration}
+                  />
+                </Form.Item>{" "}
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Status">
+                  <Input
+                    placeholder="Status"
+                    value={this.state.status}
+                    onChange={this.onChangeStatus}
+                  />
+                </Form.Item>{" "}
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Config Id">
+                  <Input
+                    placeholder="ConfigId"
+                    value={this.state.configId}
+                    onChange={this.onChangeconfigId}
+                  />
+                </Form.Item>{" "}
+              </Col>
+            </Row>
+          </Form>
         </Modal>
       </div>
     );
   }
 }
-
--- src/components/ProjectComponent/Model.js
+++ src/components/ProjectComponent/Model.js
@@ -1,45 +1,141 @@
-import { Modal, Button, Form, Row, Col, Input, Checkbox} from 'antd';
-import React from 'react';
+import {
+  Modal,
+  Button,
+  Form,
+  Row,
+  Col,
+  Input,
+  Checkbox,
+  DatePicker,
+  TimePicker
+} from "antd";
+import React from "react";
+import react, { Component } from "react";
+import axios from "axios";
 
 function onChange(e) {
   console.log(`checked = ${e.target.checked}`);
 }
 export default class Model extends React.Component {
-  state = {
-   
-    disabled: true,
-    visible: false,
-  };
+  constructor(props) {
+    super(props);
 
+    this.setState({
+      projectName: "",
+      type: "",
+      startDate: "",
+      endDate: "",
+
+      duration: "",
+      status: "",
+      configId: "",
+      visible: false
+    });
+    this.onChangeprojectName = this.onChangeprojectName.bind(this);
+    this.onChangeType = this.onChangeType.bind(this);
+    this.onChangeStartDate = this.onChangeStartDate.bind(this);
+    this.onChangeEndDate = this.onChangeEndDate.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.onChangeconfigId = this.onChangeconfigId.bind(this);
+  }
+
+  onChangeprojectName(e) {
+    this.setState({
+      projectName: e.target.value
+    });
+  }
+  onChangeType(e) {
+    this.setState({
+      type: e.target.value
+    });
+  }
+  onChangeStartDate(date, dateString) {
+    //this.setState({startDate: dateString});
+
+    this.setState({ startDate: dateString }, () =>
+      console.log(this.state.startDate)
+    );
+
+    console.log(this.state.startDate);
+  }
+  onChangeEndDate(date, dateString) {
+    this.setState({ endDate: dateString }, () =>
+      console.log(this.state.endDate)
+    );
+
+    console.log(this.state.endDate);
+  }
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+  onChangeStatus(e) {
+    this.setState({
+      status: e.target.value
+    });
+  }
+  onChangeconfigId(e) {
+    this.setState({
+      configId: e.target.value
+    });
+  }
+
+  state = {
+    disabled: true,
+    visible: false
+  };
 
   toggleDisable = () => {
     this.setState({ disabled: !this.state.disabled });
   };
 
   onChange = e => {
-    console.log('checked = ', e.target.checked);
+    console.log("checked = ", e.target.checked);
     this.setState({
-      checked: e.target.checked,
+      checked: e.target.checked
     });
   };
 
   showModal = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   handleOk = e => {
-    console.log(e);
+    e.preventDefault();
+
+    const projectData = {
+      projectName: this.state.projectName,
+      type: this.state.type,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate,
+      duration: this.state.duration,
+      status: this.state.status,
+      configId: this.state.configId
+    };
+    axios
+      .post("http://localhost:8080/project_service/createproject", projectData)
+      .then(res => console.log(res.data));
+
     this.setState({
-      visible: false,
+      projectName: "",
+      type: "",
+      startDate: "",
+      endDate: "",
+      duration: "",
+      status: "",
+      configId: "",
+      visible: false
     });
   };
 
   handleCancel = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
@@ -47,63 +143,151 @@ export default class Model extends React.Component {
     return (
       <div>
         <Button type="primary" onClick={this.showModal}>
-          Add Project 
+          Add Project
         </Button>
-        <br/>
+        <br />
         <Modal
           title="Add Project"
           visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
+          width="600px"
         >
-  <Form layout="vertical">
-<Row>
-    
-    <Col  span={24} style={{padding: "5px"}}>
-        <Form.Item label="Project Name">
-        <Input placeholder="ProjectName"/>
-         </Form.Item>{" "}
-    </Col>
-    </Row> 
-    <Row>
-    <Col>
-    <div>
-        
-        <p>
-          
-          <Checkbox
-            style={{ marginLeft: '10px' }}
-           
-            onClick={this.toggleDisable}
-            onChange={onChange}
+          <Form layout="vertical">
+            <Row>
+              <Col span={24} style={{ padding: "5px" }}>
+                <Form.Item label="Project Name">
+                  <Input
+                    placeholder="ProjectName"
+                    value={this.state.projectName}
+                    onChange={this.onChangeprojectName}
+                  />
+                </Form.Item>{" "}
+              </Col>
+            </Row>
 
-          >Edit
-            {!this.state.disabled }
-          </Checkbox>
-        
-        </p>
+            <Row>
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Type">
+                  <Input
+                    placeholder="Type"
+                    value={this.state.type}
+                    onChange={this.onChangeType}
+                  />
+                </Form.Item>{" "}
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Start Date" style={{ marginBottom: 0 }}>
+                  <span
+                    style={{
+                      display: "inline-block",
+                      width: "24px",
+                      textAlign: "center"
+                    }}
+                  />
+                  <Form.Item
+                    style={{
+                      display: "inline-block",
+                      width: "calc(90% - 12px)"
+                    }}
+                  >
+                    <DatePicker onChange={this.onChangeStartDate} />
+                  </Form.Item>
+                </Form.Item>
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="End Date" style={{ marginBottom: 0 }}>
+                  <span
+                    style={{
+                      display: "inline-block",
+                      width: "24px",
+                      textAlign: "center"
+                    }}
+                  />
+                  <Form.Item
+                    style={{
+                      display: "inline-block",
+                      width: "calc(90% - 12px)"
+                    }}
+                  >
+                    <DatePicker
+                      // value={this.state.endDate}
+                      onChange={this.onChangeEndDate}
+                    />
+                  </Form.Item>
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Duration">
+                  <Input
+                    placeholder="Duration"
+                    value={this.state.duration}
+                    onChange={this.onChangeDuration}
+                  />
+                </Form.Item>{" "}
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Status">
+                  <Input
+                    placeholder="Status"
+                    value={this.state.status}
+                    onChange={this.onChangeStatus}
+                  />
+                </Form.Item>{" "}
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Config Id">
+                  <Input
+                    placeholder="ConfigId"
+                    value={this.state.configId}
+                    onChange={this.onChangeconfigId}
+                  />
+                </Form.Item>{" "}
+              </Col>
+            </Row>
+
+            {/* <Row>
+              <Col>
+                <div>
+
+                  <p>
+
+                    <Checkbox
+                      style={{ marginLeft: '10px' }}
+
+                      onClick={this.toggleDisable}
+                      onChange={onChange}
+
+                    >Edit
+            {!this.state.disabled}
+                    </Checkbox>
+
+                  </p>
+                </div>
+              </Col>
+
+
+              <Col span={24} style={{ padding: "5px" }}>
+
+                <Form.Item>
+                  <Input
+                    disabled={this.state.disabled}
+                    onChange={this.onChange}
+                    placeholder="Abbrevation" />
+                </Form.Item>{" "}
+              </Col>
+
+            </Row> */}
+          </Form>
+        </Modal>
       </div>
-    </Col>
-  
-    
-    <Col  span={24} style={{padding: "5px"}}>
-    
-        <Form.Item>
-        <Input 
-        disabled={this.state.disabled}
-        onChange={this.onChange}
-        placeholder="Abbrevation"   />
-         </Form.Item>{" "}
-    </Col>
-
-   </Row>
-  
-      </Form>
-      
-     </Modal>
-     </div>
     );
   }
 }
-
-
