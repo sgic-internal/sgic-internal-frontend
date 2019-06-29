6f0def1055b47d5ff6d4b8baf4f51650095b7717
unknown <varnitha94@gmail.com>
2019-06-29 14:18:50 +0530

    form bug fixed

--- src/components/SettingComponent/Config/DefectTypeConfig.js
+++ src/components/SettingComponent/Config/DefectTypeConfig.js
@@ -359,7 +359,7 @@ export default class DefectTypeConfic extends React.Component {
               }}>
 
               <Form labelCol={{ span: 5 }} wrapperCol={{ span: 12 }} >
-                <Form.Item label="TypeName">
+                <Form.Item label="DefectType">
                   <Input type="text"
                     className="form-control"
                     value={this.state.name}
@@ -405,7 +405,7 @@ export default class DefectTypeConfic extends React.Component {
 
               }}>
               <Form labelCol={{ span: 5 }} wrapperCol={{ span: 12 }} onSubmit={this.handleSubmit}>
-                <Form.Item label="TypeName">
+                <Form.Item label="DefectType">
                   <Input type="text"
                     className="form-control"
                     value={this.state.name}
