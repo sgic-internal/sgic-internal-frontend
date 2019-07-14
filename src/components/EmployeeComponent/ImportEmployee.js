a4c66940533a9c93400f14c34e85de319263d966
ramz <ramminarayanan7@gmail.com>
2019-07-14 13:12:17 +0530

    Import Employees sucessfully

--- src/components/EmployeeComponent/ImportEmployee.js
+++ src/components/EmployeeComponent/ImportEmployee.js
@@ -1,139 +1,75 @@
 import React from "react";
 import { Modal, Button, message, Upload, Icon } from "antd";
-import reqwest from 'reqwest';
 
+const props = {
+  name: "uploadfile",
+  action: "http://localhost:8084/employeeservice/database",
+  headers: {
+    authorization: "authorization-text"
+  }
+};
 export default class ImportEmployee extends React.Component {
-  state = {
-    loading: false,
-    visible: false,
-    fileList: [],
-    uploading: false,
-  };
-
-  handleUpload = () => {
-    const { fileList } = this.state;
-    const formData = new FormData();
-    fileList.forEach(file => {
-      formData.append('files[]', file);
-    });
-
-    this.setState({
-      uploading: true,
-    });
-
-    // You can use any AJAX library you like
-    reqwest({
-      url: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-      method: 'post',
-      processData: false,
-      data: formData,
-      success: () => {
-        this.setState({
-          fileList: [],
-          uploading: false,
-        });
-        message.success('upload successfully.');
-      },
-      error: () => {
-        this.setState({
-          uploading: false,
-        });
-        message.error('upload failed.');
-      },
-    });
-  };
-
+  state = { visible: false, value: undefined };
   showModal = () => {
     this.setState({
       visible: true
     });
   };
-
-  handleOk = () => {
-    this.setState({ loading: true });
-    setTimeout(() => {
-      this.setState({ loading: false, visible: false });
-    }, 3000);
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  }
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
   };
 
-  handleCancel = () => {
-    this.setState({ visible: false });
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
   };
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        console.log("Received values of form: ", values);
       }
     });
   };
 
   render() {
-    const { uploading, fileList } = this.state;
-    const props = {
-      onRemove: file => {
-        this.setState(state => {
-          const index = state.fileList.indexOf(file);
-          const newFileList = state.fileList.slice();
-          newFileList.splice(index, 1);
-          return {
-            fileList: newFileList,
-          };
-        });
-      },
-      beforeUpload: file => {
-        this.setState(state => ({
-          fileList: [...state.fileList, file],
-        }));
-        return false;
-      },
-      fileList,
-    };
-    const { visible } = this.state;
+    //const {props}=this.props;
     return (
-      <div>
+      <React.Fragment>
         <Button type="primary" onClick={this.showModal}>
-          Import Employee
+          <Icon />
+          Import Employees
         </Button>
+
         <Modal
-          visible={visible}
-          title="Import Employee"
+          title="Import Employees"
+          visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
-          footer={[
-            <Button key="back" onClick={this.handleCancel}>
-              Return
-            </Button>,
-           
-            <Button
-              type="primary"
-              onClick={this.handleUpload}
-              disabled={fileList.length === 0}
-              loading={uploading}
-              style={{ marginTop: 16 }}
-            >
-              {uploading ? 'Uploading' : 'Start Upload'}
-            </Button>
-          ]}
+          width="600px"
         >
           <Upload {...props}>
-           
-           <p style={{ color:"red", margin:"0 ", display:"inline"}} >* </p> <p style={{argin:"0 ", display:"inline"}} align="right">Upload .xls file only.</p>
-          <br></br>
-         
-         
-          <br>
-          </br>
             <Button>
-              <Icon type="upload" /> Select File
-          </Button>
+              <Icon type="upload" /> Click to Upload
+            </Button>
           </Upload>
         </Modal>
-
-
-      </div>
+      </React.Fragment>
     );
   }
 }
-
-
